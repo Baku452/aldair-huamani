a86d0839cec9f85f8393bdce1c6e0cb514248a8e
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2026-03-27 00:08:47 -0500

    Add skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.aldairhuamani.com',
-integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   adapter: vercel(),
 });
